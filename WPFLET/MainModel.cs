f9b3b4520243afd319ee0f4b2ae3c22c73342f1a
백승민 <qor5457@gmail.com>
2023-01-11 18:00:15 +0900

    1

---
+++ WPFLET/MainModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace WPFLET
+{
+    public class MainModel
+    {
+        [RelayCommand]
+        public void BtnNA()
+        {
+                
+        }
+    }
+}
