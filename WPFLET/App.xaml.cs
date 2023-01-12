7f743f834721ab57f698deed636f9fb9e0a74d06
백승민 <qor5457@gmail.com>
2023-01-12 14:26:40 +0900

    Microsoft.Extensions.DependencyInjection.Extensions 적용

--- WPFLET/App.xaml.cs
+++ WPFLET/App.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication.ExtendedProtection;
 using System.Threading.Tasks;
 using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace WPFLET
 {
@@ -13,5 +19,22 @@ namespace WPFLET
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+        
+        public new static App Current => (App)Application.Current;
+        
+        public IServiceProvider Services { get; }
+        
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            
+            return services.BuildServiceProvider();
+        }
     }
 }
